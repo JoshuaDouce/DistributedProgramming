01ee50d6557c794ec80576fb5883288ee86f364e
JoshuaDouce <Joshuadouce@gmail.com>
2019-11-26 21:40:33 +0000

    BotApi added

---
+++ BotApi/Controllers/RandomResponseController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomResponseController : ControllerBase
+    {
+        // GET: api/RandomResponse
+        [HttpGet]
+        public string Get()
+        {
+            var random = new Random();
+
+            var x = random.Next(0, 5);
+
+            switch (x)
+            {
+                case 0:
+                    return "Ranom message 1";
+                case 1:
+                    return "Ranom message 2";
+                case 2:
+                    return "Ranom message 3";
+                case 3:
+                    return "Ranom message 4";
+                case 4:
+                    return "Ranom message 5";
+                default:
+                    return "Ranom message 6";
+            }
+        }
+    }
+}
--- SignalRWithAuth/Hubs/ChatHub.cs
+++ SignalRWithAuth/Hubs/ChatHub.cs
@@ -1,13 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRWithAuth.Hubs
 {
     public class ChatHub : Hub
     {
+        static readonly HttpClient client = new HttpClient();
+
         public async Task SendMessage(string user, string message)
         {
+            var url = "https://localhost:44306/api/randomresponse";
+
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", "Bot", responseBody);
         }
     }
 }
